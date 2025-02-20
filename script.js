704155f7ed4aef2c9690f16246e5c4948bc1bc3c
najwazzahra <najwaazzahra144@gmail.com>
2025-02-20 18:21:24 +0700

    hapus tanda kutip di js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("hello dari user atau pengguna lain")
 let umur = 20;
 
 if (umur < 18) {
-    console.log{"gaboleh masuk!"}
+    console.log{gaboleh masuk!}
 } else {
     console.log("oke")
 }
